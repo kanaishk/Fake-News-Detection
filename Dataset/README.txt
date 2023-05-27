6a1dd21a07cb6c6da5e110468a6ba900ad884d63
kanaishk <kanaishkgarg@gmail.com>
2023-05-27 22:35:31 +0530

    about to start base trials

--- Dataset/README.txt
+++ Dataset/README.txt
@@ -1,5 +1,11 @@
 Problem Description
 The authenticity of Information has become a longstanding issue affecting businesses and society, both for printed and digital media. On social networks, the reach and effects of information spread occur at such a fast pace and so amplified that distorted, inaccurate, or false information acquires a tremendous potential to cause real-world impacts, within minutes, ffor millions of users. Recently, several public concerns about this problem and some approaches to mitigate the problem were expressed.
 
+https://ieeexplore.ieee.org/document/9395133
+
 Data- Description:
-There are 6 columns in the dataset provided to you. The description of each of the column is given below: “id”: Unique id of each news article “headline”: It is the title of the news. “news”: It contains the full text of the news article “Unnamed:0”: It is a serial number “written_by”: It represents the author of the news article “label”: It tells whether the news is fake (1) or not fake (0).
+There are 4 columns in the dataset provided to you. The description of each of the column is given below: 
+“Unnamed:0”: It is a serial number 
+“headline”: It is the title of the news. 
+“news”: It contains the full text of the news article 
+“label”: It tells whether the news is fake (1) or not fake (0).
--- Model_BASE.py
+++ Model_BASE.py
@@ -3,7 +3,7 @@ import random
 import pandas as pd
 import numpy as np 
 from sklearn.model_selection import RandomizedSearchCV, train_test_split
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
@@ -11,13 +11,14 @@ from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.metrics import make_scorer, accuracy_score, f1_score, recall_score, precision_score
 from sklearn.metrics import classification_report
+from imblearn.under_sampling import RandomUnderSampler
 
 cwd = os.getcwd()
 dataset_dir = os.path.join(cwd,'Dataset')
-result_dir = os.path.join(cwd,'Results/Count')
+result_dir = os.path.join(cwd,'Results')
 df = pd.read_csv(os.path.join(dataset_dir,'train_news_preprocessed.csv'), low_memory=False, 
-                 usecols = ['label','clean_news_tokens','clean_headline_tokens'])
-# 'headline','news','headline_len','news_len','caps_in_headline','caps_in_news',
+                 usecols = ['label','clean_news_tokens'])
+# ,'clean_headline_tokens','headline','news','headline_len','news_len','caps_in_headline','caps_in_news'
 
 RANDOM_STATE = 1973
 random.seed(RANDOM_STATE)
@@ -30,12 +31,12 @@ y = df['label']  # Target variable (fake or not fake)
 
 X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2, random_state=RANDOM_STATE)
 
-transformer = TfidfTransformer(smooth_idf=False)
-vectorizer = CountVectorizer(ngram_range=(1, 2))
-X_train_count = vectorizer.fit_transform(X_train)
-X_valid_count = vectorizer.transform(X_valid)
-#X_train_count = transformer.fit_transform(X_train_count)
-#X_valid_count = transformer.fit_transform(X_valid_count)
+vectorizer = TfidfVectorizer()
+X_train_tfidf = vectorizer.fit_transform(X_train)
+X_valid_tfidf = vectorizer.transform(X_valid)
+
+undersampler = RandomUnderSampler(random_state=RANDOM_STATE)
+X_train_tfidf, y_train = undersampler.fit_resample(X_train_tfidf, y_train)
 
 Iters = 1
 parallel_workers = 5
@@ -81,27 +82,8 @@ def model_logging(modelname, Iters, random_search, X_valid_tfidf, y_valid):
     with open(os.path.join(result_dir, f'BASE/BASE_{modelname}_Valid_report.txt'), 'w') as f:
         f.write(report)
 
-RFC_para = {
-    'random_state': [RANDOM_STATE]
-}
-
 random_search = RandomizedSearchCV(
-    estimator=RandomForestClassifier(),
-    param_distributions=RFC_para,
-    n_iter=Iters,
-    n_jobs=parallel_workers,
-    cv=cross_val_works,
-    scoring=scorers,
-    refit='accuracy',
-    return_train_score=True,
-    verbose=verbose
-)
-
-random_search.fit(X_train_count, y_train)
-model_logging('RFC',Iters,random_search,X_valid_count,y_valid)
-
-random_search = RandomizedSearchCV(
-    estimator=KNeighborsClassifier(),
+    estimator=MultinomialNB(),
     param_distributions={},
     n_iter=Iters,
     n_jobs=parallel_workers,
@@ -112,8 +94,8 @@ random_search = RandomizedSearchCV(
     verbose=verbose
 )
 
-random_search.fit(X_train_count, y_train)
-model_logging('KNN',Iters,random_search,X_valid_count,y_valid)
+random_search.fit(X_train_tdidf, y_train)
+model_logging('MNB',Iters,random_search,X_valid_tdidf,y_valid)
 
 LR_para = {
     'random_state': [RANDOM_STATE]
@@ -131,12 +113,16 @@ random_search = RandomizedSearchCV(
     verbose=verbose
 )
 
-random_search.fit(X_train_count, y_train)
-model_logging('LR',Iters,random_search,X_valid_count,y_valid)
+random_search.fit(X_train_tdidf, y_train)
+model_logging('LR',Iters,random_search,X_valid_tdidf,y_valid)
+
+RFC_para = {
+    'random_state': [RANDOM_STATE]
+}
 
 random_search = RandomizedSearchCV(
-    estimator=MultinomialNB(),
-    param_distributions={},
+    estimator=RandomForestClassifier(),
+    param_distributions=RFC_para,
     n_iter=Iters,
     n_jobs=parallel_workers,
     cv=cross_val_works,
@@ -146,8 +132,8 @@ random_search = RandomizedSearchCV(
     verbose=verbose
 )
 
-random_search.fit(X_train_count, y_train)
-model_logging('MNB',Iters,random_search,X_valid_count,y_valid)
+random_search.fit(X_train_tdidf, y_train)
+model_logging('RFC',Iters,random_search,X_valid_tdidf,y_valid)
 
 GBC_para = {
     'random_state': [RANDOM_STATE]
@@ -165,8 +151,8 @@ random_search = RandomizedSearchCV(
     verbose=verbose
 )
 
-random_search.fit(X_train_count, y_train)
-model_logging('GBC',Iters,random_search,X_valid_count,y_valid)
+random_search.fit(X_train_tdidf, y_train)
+model_logging('GBC',Iters,random_search,X_valid_tdidf,y_valid)
 
 SVM_para = {
     'random_state': [RANDOM_STATE]
@@ -184,5 +170,5 @@ random_search = RandomizedSearchCV(
     verbose=verbose
 )
 
-random_search.fit(X_train_count, y_train)
-model_logging('SVM',Iters,random_search,X_valid_count,y_valid)
+random_search.fit(X_train_tdidf, y_train)
+model_logging('SVM',Iters,random_search,X_valid_tdidf,y_valid)
